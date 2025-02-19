626099d20f7ad31939ec0dab43147aecf81e6170
Andy Zhen <azhen2603@gmail.com>
2025-02-18 21:32:42 -0500

    chore: lay out ServoController, MotionController

---
+++ src/motion.py
@@ -0,0 +1,18 @@
+class MotionController():
+    def __init__(self) -> None:
+        pass
+
+    def start(speed: float):
+        pass
+
+    def stop():
+        pass
+
+    def move(distance: float):
+        pass
+
+    def set_motor_speed(speed: float):
+        pass
+
+    def turn(rotation_deg: float):
+        pass
---
+++ src/servo.py
@@ -0,0 +1,6 @@
+class ServoController():
+    def __init__(self) -> None:
+        pass
+
+    def set_servo_state(self, is_active: bool):
+        pass
