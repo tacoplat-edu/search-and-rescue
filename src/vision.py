7adbd164f01aec91a3d2d1564939c74ad47c4e8f
Andy Zhen <azhen2603@gmail.com>
2025-02-18 21:34:52 -0500

    chore: add visionprocessor class

---
+++ src/vision.py
@@ -0,0 +1,12 @@
+class VisionProcessor:
+    def __init__(self):
+        pass
+
+    def get_path_mask():
+        pass
+
+    def get_path_curvature():
+        pass
+
+    def run():
+        pass
