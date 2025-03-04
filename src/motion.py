48a87e44559641c928c0d9bb5280ecb8f6b941b7
j2002chen <j.2002chen@gmail.com>
2025-03-03 23:23:51 -0500

    chore: inital implementation of MotionController class

--- src/motion.py
+++ src/motion.py
@@ -1,18 +1,49 @@
+import math 
+
+from models.devices import devices
+from models.wheel import Wheel
+
 class MotionController():
     def __init__(self) -> None:
-        pass
+        self.left_motor = devices.wheel_motors[Wheel.LEFT]
+        self.right_motor = devices.wheel_motors[Wheel.RIGHT]
+        self.wheel_circumference = 2 * math.pi * 2 # 20 mm radius
 
-    def start(speed: float):
-        pass
+    def set_motor_speed(self, speed: float):
+        self.left_motor.forward(speed)
+        self.right_motor.forward(speed)
 
-    def stop():
-        pass
+    def set_reverse_speed(self, speed: float):
+        self.left_motor.backward(speed)
+        self.right_motor.backward(speed)
 
-    def move(distance: float):
-        pass
+    def set_right_turn_speed(self, speed: float):
+        self.left_motor.forward(speed)
+        self.right_motor.backward(speed)
 
-    def set_motor_speed(speed: float):
-        pass
+    def set_left_turn_speed(self, speed: float):
+        self.left_motor.backward(speed)
+        self.right_motor.forward(speed)
+        
+    def start(self, speed: float):
+        self.set_motor_speed(speed)
 
-    def turn(rotation_deg: float):
-        pass
+    def stop(self):
+        self.left_motor.stop()
+        self.right_motor.stop()
+
+    def move(self, distance: float):
+        rotations_needed = distance / self.wheel_circumference
+        current_rotations = 0
+        while True:
+            self.left_motor.forward()
+            self.right_motor.forward()
+            if current_rotations >= rotations_needed:
+                break
+        self.stop()
+
+    def turn(self, rotation_deg: float, speed: float):
+        if rotation_deg > 0:
+            self.set_right_turn_speed(speed)
+        else:
+            self.set_left_turn_speed(speed)
