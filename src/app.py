cbb15895eadf68c3f3caeaa059387954aaa62725
j2002chen <j.2002chen@gmail.com>
2025-03-03 23:26:49 -0500

    chore: added test run of GPIO and motors

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from models.devices import devices
+from servo import ServoController
+from motion import MotionController
+
+gripper = ServoController(pin=17)
+motion = MotionController()
+button = devices.action_button
+
+def test_run():
+    motion.start(speed=0.5)
+    gripper.grip()
+    motion.stop()
+    gripper.release_grip()
+    motion.turn()
+    
+
+
+button.when_pressed = test_run
+
