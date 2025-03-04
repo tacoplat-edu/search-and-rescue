2e4cbebf25d8c2cb48bfd217dd15095d4fbe5f6f
James Chen <j.2002chen@gmail.com>
2025-03-04 18:14:30 -0500

    fix: removed extra comma

--- src/helpers/vision.py
+++ src/helpers/vision.py
@@ -8,5 +8,5 @@ NUM_DOTS = 3
 def get_dot_locations(frame_height: int):
     res = []
     for i in range(1, NUM_DOTS+1):
-        res.append(int(frame_height * (i / (NUM_DOTS+1))),)
+        res.append(int(frame_height * (i / (NUM_DOTS+1))))
     return res
