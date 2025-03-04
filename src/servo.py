8bc654734e523b85edc93090bd23fc119f6960b9
j2002chen <j.2002chen@gmail.com>
2025-03-03 23:24:36 -0500

    chore: initial implementation of ServoController class

--- src/servo.py
+++ src/servo.py
@@ -1,7 +1,23 @@
+from models.devices import devices 
+
 class ServoController():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, pin) -> None:
+        self.active_flag = False
+        self.servo = devices.servo_motor
+        self.servo.angle = 0  # Initialize the servo angle
 
     def set_servo_state(self, is_active: bool):
-        pass
-    
+        self.active_flag = is_active
+
+    def set_servo_angle(self, angle):
+        self.servo.angle = angle
+
+    def release_grip(self):
+        if self.active_flag == True:
+            self.set_servo_angle(0)
+            self.set_servo_state(False)
+
+    def grip(self):
+        if self.active_flag == False:
+            self.set_servo_angle(90)
+            self.set_servo_state(True)
