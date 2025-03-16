cdc410502279ace4236cc89c998109faa348a784
j2002chen <j.2002chen@gmail.com>
2025-03-16 01:13:10 -0400

    WIP: increase min speed

--- src/vision.py
+++ src/vision.py
@@ -17,7 +17,7 @@ PX_TO_CM = 13 / 640
 CORRECTION_SCALE_FACTOR = 0.01
 SHOW_IMAGES = os.environ.get("SHOW_IMAGE_WINDOW") == "true"
 MAX_CORRECITON = 0.12
-MIN_SPEED = 0.2
+MIN_SPEED = 0.22
 MAX_SPEED = 0.26
 
 class VisionProcessor:
