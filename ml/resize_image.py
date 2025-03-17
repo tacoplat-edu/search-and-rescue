5b826cb67e9798578484c02260d57d59c8d71545
j2002chen <j.2002chen@gmail.com>
2025-03-17 16:55:33 -0400

    chore: script to resize images for model training

---
+++ ml/resize_image.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+
+input_dir = "background_iamges"
+output_dir = "resized_background_images"
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+for filename in os.listdir(input_dir):
+    if filename.endswith(".jpg"):
+        image_path = os.path.join(input_dir, filename)
+        image = cv2.imread(image_path)
+        resized_image = cv2.resize(image, (640, 480))
+        output_path = os.path.join(output_dir, filename)
+        cv2.imwrite(output_path, resized_image)
