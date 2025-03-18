073d8f1265a87eae3a591524b5e830ab2709d907
j2002chen <j.2002chen@gmail.com>
2025-03-18 00:15:05 -0400

    chore: update export parameters

--- ml/convert_weights.py
+++ ml/convert_weights.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 
 # Load your model
-model = YOLO("../runs/detect/train5/weights/best.pt")
+model = YOLO("../runs/detect/train/weights/best.pt")
 
 # Export the model
-model.export(format="onnx")
+model.export(format="onnx",
+             opset=11,        
+             simplify=True,     
+             dynamic=False,  
+             half=False) 
