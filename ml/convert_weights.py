cbbb385f9e675cfcf697e944ac7fe048db4bea29
j2002chen <j.2002chen@gmail.com>
2025-03-18 21:57:06 -0400

    WIP: kms

--- ml/convert_weights.py
+++ ml/convert_weights.py
@@ -6,6 +6,9 @@ model = YOLO("../runs/detect/train/weights/best.pt")
 # Export the model
 model.export(format="onnx",
              opset=12,        
+             imgsz = 640,
              simplify=True,     
-             dynamic=False,  
+             dynamic=False,
+             do_constant_folding=False,  
+             nms=False,
              half=False) 
