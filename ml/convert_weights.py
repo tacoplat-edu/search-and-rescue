d3def4ce8035ad97c1b71586e8b6a4ae8eaead03
j2002chen <j.2002chen@gmail.com>
2025-03-18 22:15:25 -0400

    WIP: D:

--- ml/convert_weights.py
+++ ml/convert_weights.py
@@ -5,10 +5,9 @@ model = YOLO("../runs/detect/train/weights/best.pt")
 
 # Export the model
 model.export(format="onnx",
-             opset=12,        
+             opset=9,        
              imgsz = 640,
              simplify=True,     
              dynamic=False,
-             do_constant_folding=False,  
              nms=False,
              half=False) 
