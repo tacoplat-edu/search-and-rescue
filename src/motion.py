d56b2d38b0b2faeeeb90a628e1cfa103cc497f8e
j2002chen <j.2002chen@gmail.com>
2025-03-23 23:04:35 -0400

    chore: moving tuning parameters to separate files

--- src/motion.py
+++ src/motion.py
@@ -5,6 +5,8 @@ import os
 from models.devices import DeviceConfiguration
 from models.wheel import Wheel
 from helpers.motion import assert_speed
+from parameters.class_param import DEFAULT_SPEED 
+#from parameters.home_param import DEFAULT_SPEED
 
 DEBUG = os.environ.get("DEBUG") == "true"
 
@@ -26,7 +28,7 @@ class MotionController:
         self.wheel_diameter = 7
         self.wheel_distance = 18.25
         self.wheel_circumference = 2 * math.pi * self.wheel_diameter/2  
-        self.default_speed = 0.23
+        self.default_speed = DEFAULT_SPEED
 
     def reset_encoders(self):
         self.devices.wheel_encoders[Wheel.LEFT].steps = 0
