02c3ca18398902cfb18fe73f3e24f29a6bf45548
j2002chen <j.2002chen@gmail.com>
2025-03-24 16:57:41 -0400

    af

--- src/models/devices.py
+++ src/models/devices.py
@@ -30,7 +30,7 @@ devices = (
             Wheel.LEFT: RotaryEncoder(26,25, max_steps=9000),
             Wheel.RIGHT: RotaryEncoder(23,24, max_steps=9000),
         },
-        servo_motor=AngularServo(9, min_angle=-90, max_angle=90),
+        servo_motor=AngularServo(13, min_angle=-90, max_angle=90),
         action_button=Button(16, pull_up=True),
     )
 )
