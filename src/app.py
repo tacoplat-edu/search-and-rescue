c504f1483f17ed2307ed8afa67dc6127d9d06ecf
j2002chen <j.2002chen@gmail.com>
2025-03-24 21:29:19 -0400

    adsf

--- src/app.py
+++ src/app.py
@@ -52,17 +52,38 @@
 # else:
 #     pass """
 # pause()
-from gpiozero import Servo
-from time import sleep
-servo = Servo(13)  # GPIO pin number (BCM numbering)
+# from gpiozero import Servo
+# from time import sleep
+# servo = Servo(13)  # GPIO pin number (BCM numbering)
+
+# try:
+#     while True:
+#         servo.mid()
+#         sleep(1)
+#         servo.min()
+#         sleep(1)
+#         servo.max()
+#         sleep(1)
+#         print("powering servo")
+# except KeyboardInterrupt:
+#     print("Exiting....")
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.OUT)
+
+pwm = GPIO.PWM(13, 50)  # 50Hz for standard servo
+pwm.start(7.5)  # Middle position (duty cycle ~7.5%)
 
 try:
     while True:
-        servo.mid()
-        sleep(1)
-        servo.min()
-        sleep(1)
-        servo.max()
-        sleep(1)
+        pwm.ChangeDutyCycle(7.5)  # Middle
+        time.sleep(1)
+        pwm.ChangeDutyCycle(5.0)  # Min
+        time.sleep(1)
+        pwm.ChangeDutyCycle(10.0) # Max
+        time.sleep(1)
 except KeyboardInterrupt:
-    print("Exiting....")
+    pwm.stop()
+    GPIO.cleanup()
