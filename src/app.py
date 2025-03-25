c6f469060038aa5f0d976e08ec293a728ad4459d
j2002chen <j.2002chen@gmail.com>
2025-03-24 20:07:17 -0400

    asdf

--- src/app.py
+++ src/app.py
@@ -54,10 +54,7 @@
 # pause()
 from gpiozero import Servo
 from time import sleep
-from gpiozero.pins.pigpio import PiGPIOFactory
-pigpio_factory = PiGPIOFactory()
-
-servo = Servo(13, pin_factory=pigpio_factory)  # GPIO pin number (BCM numbering)
+servo = Servo(13)  # GPIO pin number (BCM numbering)
 
 try:
     while True:
