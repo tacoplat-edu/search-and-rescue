203402452dfc363e571322ae7bbe7371706642c4
jchen1707 <j.2002chen@gmail.com>
2025-03-25 18:44:38 -0400

    wip: revert to line follow test

--- src/app.py
+++ src/app.py
@@ -1,130 +1,76 @@
-# import os
-# #import keyboard
-# from signal import pause
+import os
+#import keyboard
+from signal import pause
 
-# os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
-# os.environ["SHOW_IMAGE_WINDOW"] = "false"
-# import cv2
-# from dotenv import load_dotenv
-# load_dotenv()
+os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
+os.environ["SHOW_IMAGE_WINDOW"] = "false"
+import cv2
+from dotenv import load_dotenv
+load_dotenv()
 
-# from vision import VisionProcessor
-# from servo import ServoController
-# from motion import MotionController
+from vision import VisionProcessor
+from servo import ServoController
+from motion import MotionController
 
-# from models.devices import devices
+from models.devices import devices
 
-# motion = MotionController(devices)
-# servo = ServoController()
+motion = MotionController(devices)
+servo = ServoController()
 
-# vp = VisionProcessor(
-#     motion,
-#     servo,
-#     {
-#         cv2.CAP_PROP_FRAME_WIDTH: 640,
-#         cv2.CAP_PROP_FRAME_HEIGHT: 480,
-#     },
-# )
+vp = VisionProcessor(
+    motion,
+    servo,
+    {
+        cv2.CAP_PROP_FRAME_WIDTH: 640,
+        cv2.CAP_PROP_FRAME_HEIGHT: 480,
+    },
+)
 
-# button = devices.action_button
-# def press_handler():
-#    print("what the sigma")
-#    vp.run()
-#    #vp.running = False
-# button.when_pressed = press_handler
+button = devices.action_button
+def press_handler():
+   print("what the sigma")
+   vp.run()
+   #vp.running = False
+button.when_pressed = press_handler
 
-# #vp.run()
-# """ 
-# if os.environ.get("DEBUG") == "true":
-#     try:
-#         while True:
-#             state = button.pin.state
-#             if keyboard.read_key() == "f":
-#                 if state:
-#                     button.pin.drive_low()
-#                 else:
-#                     button.pin.drive_high()
-#     except KeyboardInterrupt:
-#         print("Stopped")
-#     finally:
-#         devices.servo_motor.close()
-#         devices.action_button.close()
-# else:
-#     pass """
-# pause()
-# import os
-# #import keyboard
-# from signal import pause
+#vp.run()
+""" 
+if os.environ.get("DEBUG") == "true":
+    try:
+        while True:
+            state = button.pin.state
+            if keyboard.read_key() == "f":
+                if state:
+                    button.pin.drive_low()
+                else:
+                    button.pin.drive_high()
+    except KeyboardInterrupt:
+        print("Stopped")
+    finally:
+        devices.servo_motor.close()
+        devices.action_button.close()
+else:
+    pass """
+pause()
 
-# os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
-# os.environ["SHOW_IMAGE_WINDOW"] = "false"
-# import cv2
-# from dotenv import load_dotenv
-# load_dotenv()
+# from gpiozero import Servo, AngularServo
+# from gpiozero.pins.lgpio import LGPIOFactory
 
-# from vision import VisionProcessor
-# from servo import ServoController
-# from motion import MotionController
-
-# from models.devices import devices
-
-# motion = MotionController(devices)
-# servo = ServoController()
-
-# vp = VisionProcessor(
-#     motion,
-#     servo,
-#     {
-#         cv2.CAP_PROP_FRAME_WIDTH: 640,
-#         cv2.CAP_PROP_FRAME_HEIGHT: 480,
-#     },
-# )
-
-# button = devices.action_button
-# def press_handler():
-#    print("what the sigma")
-#    vp.run()
-#    #vp.running = False
-# button.when_pressed = press_handler
-
-# #vp.run()
-# """ 
-# if os.environ.get("DEBUG") == "true":
-#     try:
-#         while True:
-#             state = button.pin.state
-#             if keyboard.read_key() == "f":
-#                 if state:
-#                     button.pin.drive_low()
-#                 else:
-#                     button.pin.drive_high()
-#     except KeyboardInterrupt:
-#         print("Stopped")
-#     finally:
-#         devices.servo_motor.close()
-#         devices.action_button.close()
-# else:
-#     pass """
-# pause()
-from gpiozero import Servo, AngularServo
-from gpiozero.pins.lgpio import LGPIOFactory
-
-from time import sleep
-factory = LGPIOFactory(chip=0)
-#servo = Servo(18,pin_factory=factory)  # GPIO pin number (BCM numbering)
-angularservo = AngularServo(18,pin_factory=factory)
-try:
-    while True:
-        angularservo.angle = 20
-        print("min")
-        sleep(1)
-        angularservo.angle = 45
-        print("mid")
-        sleep(1)
-        angularservo.angle = 70
-        print("max")
-        sleep(1)
-        print("powering servo")
-except KeyboardInterrupt:
-    print("Exiting....")
+# from time import sleep
+# #servo = Servo(18,pin_factory=factory)  # GPIO pin number (BCM numbering)
+# angularservo = AngularServo(18, max_angle=90, min_angle=0)
+# try:
+#     while True:
+#         angularservo.min()
+#         print("min", angularservo.value)
+#         #sleep(1)
+#         #angularservo.mid()
+#         #print("mid", angularservo.value)
+#         #sleep(1)
+#         #angularservo.max()
+#         #print("max", angularservo.value)
+#         sleep(1)
+#         print("powering servo")
+# except KeyboardInterrupt:
+#     print("Exiting....")
 
--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-import os
-os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
-import cv2
+import time
+from gpiozero import AngularServo
 
-while True:
-    capture = cv2.VideoCapture(0)
+s = AngularServo(13, min_angle=0, max_angle=180)
 
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-    _, frame = capture.read()
-
-    cv2.imshow("Image", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+try:
+    while True:
+        s.max()
+        print("max")
+        time.sleep(2)
+        s.min()
+        print("min")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("exiting")
